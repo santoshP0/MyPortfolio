625415f7cc5a6ea8f207e93051bfe7a86d231a5d
santoshP0 <santoshpk.mdev@gmail.com>
2026-02-20 18:31:07 +0530

    feat(01-core-mechanics-02): Implement Mouse Look
    - Added PointerLockControls to Scene.jsx to enable mouse-look.

--- src/game/Scene.jsx
+++ src/game/Scene.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Physics } from "@react-three/rapier";
+import { PointerLockControls } from "@react-three/drei";
 import Ground from "./Ground";
 import { Player } from "./Player";
 
 const Scene = () => {
   return (
-    <Physics debug>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
-      <Player />
-      <Ground />
-    </Physics>
+    <>
+      <Physics debug>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={1} />
+        <Player />
+        <Ground />
+      </Physics>
+      <PointerLockControls />
+    </>
   );
 };
 
