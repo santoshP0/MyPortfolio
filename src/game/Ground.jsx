c51c9693874283e8e820870feb050c9c5c240f68
santoshP0 <santoshpk.mdev@gmail.com>
2026-02-20 18:27:40 +0530

    feat(01-core-mechanics-1): add physics and ground

--- src/game/Ground.jsx
+++ src/game/Ground.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { RigidBody } from '@react-three/rapier';
-import { CuboidCollider } from '@react-three/rapier';
+import React from "react";
+import { RigidBody, CuboidCollider } from "@react-three/rapier";
 
-function Ground() {
+const Ground = () => {
   return (
-    <RigidBody type="fixed">
-      <CuboidCollider args={[100, 0.5, 100]} position={[0, -0.5, 0]} />
+    <RigidBody type="fixed" colliders={false}>
+      <mesh receiveShadow position={[0, -0.5, 0]}>
+        <boxGeometry args={[1000, 1, 1000]} />
+        <meshStandardMaterial color="lightgray" />
+      </mesh>
+      <CuboidCollider args={[500, 0.5, 500]} position={[0, -0.5, 0]} />
     </RigidBody>
   );
-}
+};
 
 export default Ground;
--- src/game/Scene.jsx
+++ src/game/Scene.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
+import Ground from "./Ground";
 
 const Scene = () => {
   return (
-    <>
+    <Physics debug>
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
-    </>
+      <Ground />
+    </Physics>
   );
 };
 
