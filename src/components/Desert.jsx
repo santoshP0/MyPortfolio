ef4a09d7446783db901737ef820a2a5039764284
santoshP0 <santoshpk.mdev@gmail.com>
2026-02-21 12:31:15 +0530

    fix(car): resolve falling issue and adjust camera control

--- src/components/Desert.jsx
+++ src/components/Desert.jsx
@@ -1,32 +1,24 @@
 import React, { memo } from "react";
 import { useTexture, Sky } from "@react-three/drei";
-import { RigidBody, CuboidCollider } from "@react-three/rapier";
+import { RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
 
 const Desert = memo((props) => {
-  const [basecolor, normalMap, heightMap] = useTexture([
+  const [basecolor] = useTexture([
     "/textures/sand_basecolor.png",
-    "/textures/sand_normal.png",
-    "/textures/sand_height.png",
   ]);
 
   // Repeat the textures
-  [basecolor, normalMap, heightMap].forEach((texture) => {
-    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(10, 10);
-  });
+  basecolor.wrapS = basecolor.wrapT = THREE.RepeatWrapping;
+  basecolor.repeat.set(10, 10);
 
   return (
     <>
       <RigidBody type="fixed" colliders="cuboid" {...props}>
         <mesh receiveShadow>
-          <planeGeometry args={[100, 100, 128, 128]} />
+          <planeGeometry args={[100, 100]} />
           <meshStandardMaterial
             map={basecolor}
-            normalMap={normalMap}
-            displacementMap={heightMap}
-            displacementScale={2}
-            flatShading={false}
           />
         </mesh>
       </RigidBody>
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -7,21 +7,6 @@ function Scene({ carRef, controlsRef }) {
   const removeBullet = useBulletStore((state) => state.removeBullet);
 
   useFrame(() => {
-    if (carRef.current && controlsRef.current) {
-      const rigidBody = carRef.current;
-      // Ensure rigidBody and its translation method are available
-      if (rigidBody && typeof rigidBody.translation === "function") {
-        try {
-          const carPosition = rigidBody.translation();
-          controlsRef.current.target.lerp(carPosition, 0.1);
-          controlsRef.current.update();
-        } catch (error) {
-          // In case of any other error within translation()
-          console.error("Error getting car translation:", error);
-        }
-      }
-    }
-
     // Bullet lifecycle management
     bullets.forEach((bullet) => {
       if (Date.now() - bullet.timestamp > 3000) {
