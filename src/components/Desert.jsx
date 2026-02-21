a1c40efde5d6ce97a60fdc030060f826197d4671
santoshP0 <santoshpk.mdev@gmail.com>
2026-02-21 11:42:25 +0530

    fix(desert): use RigidBody with CuboidCollider for ground plane

--- src/components/Desert.jsx
+++ src/components/Desert.jsx
@@ -1,11 +1,9 @@
 import React, { memo } from "react";
 import { useTexture, Sky } from "@react-three/drei";
-import { usePlane } from "@react-three/rapier";
+import { RigidBody, CuboidCollider } from "@react-three/rapier";
 import * as THREE from "three";
 
 const Desert = memo((props) => {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
-  
   const [basecolor, normalMap, heightMap] = useTexture([
     "/textures/sand_basecolor.png",
     "/textures/sand_normal.png",
@@ -20,16 +18,18 @@ const Desert = memo((props) => {
 
   return (
     <>
-      <mesh ref={ref} receiveShadow>
-        <planeGeometry args={[100, 100, 128, 128]} /> {/* Increased segments for displacement */}
-        <meshStandardMaterial
-          map={basecolor}
-          normalMap={normalMap}
-          displacementMap={heightMap}
-          displacementScale={2}
-          flatShading={false}
-        />
-      </mesh>
+      <RigidBody type="fixed" colliders="cuboid" {...props}>
+        <mesh receiveShadow>
+          <planeGeometry args={[100, 100, 128, 128]} />
+          <meshStandardMaterial
+            map={basecolor}
+            normalMap={normalMap}
+            displacementMap={heightMap}
+            displacementScale={2}
+            flatShading={false}
+          />
+        </mesh>
+      </RigidBody>
       <Sky sunPosition={[100, 20, 100]} />
     </>
   );
